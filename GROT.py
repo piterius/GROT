123d66d4382974c7c2e1cebe4fd089e9c2bf32ca
piterius <edgar.pilch@protonmail.ch>
2019-03-03 20:22:54 +0100

    changed names of variables

--- GROT.py
+++ GROT.py
@@ -6,39 +6,39 @@ def grot(matrix):
     most_moves = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            x, y = i, j
+            x, y = j, i
             moves = 0
             copy_mx = copy.deepcopy(matrix)
             prev = ''
-            while len(copy_mx) > x > -1 and len(copy_mx[x]) > y > -1:
-                if copy_mx[x][y] == 'u':
+            while len(copy_mx) > y > -1 and len(copy_mx[y]) > x > -1:
+                if copy_mx[y][x] == 'u':
                     moves += 1
-                    copy_mx[x][y] = ''
-                    x -= 1
-                    prev = 'u'
-                elif copy_mx[x][y] == 'd':
-                    moves += 1
-                    copy_mx[x][y] = ''
-                    x += 1
-                    prev = 'd'
-                elif copy_mx[x][y] == 'r':
-                    moves += 1
-                    copy_mx[x][y] = ''
-                    y += 1
-                    prev = 'r'
-                elif copy_mx[x][y] == 'l':
-                    moves += 1
-                    copy_mx[x][y] = ''
+                    copy_mx[y][x] = ''
                     y -= 1
+                    prev = 'u'
+                elif copy_mx[y][x] == 'd':
+                    moves += 1
+                    copy_mx[y][x] = ''
+                    y += 1
+                    prev = 'd'
+                elif copy_mx[y][x] == 'r':
+                    moves += 1
+                    copy_mx[y][x] = ''
+                    x += 1
+                    prev = 'r'
+                elif copy_mx[y][x] == 'l':
+                    moves += 1
+                    copy_mx[y][x] = ''
+                    x -= 1
                     prev = 'l'
                 elif prev == 'd':
-                    x += 1
-                elif prev == 'r':
                     y += 1
+                elif prev == 'r':
+                    x += 1
                 elif prev == 'l':
-                    y -= 1
-                else:
                     x -= 1
+                else:
+                    y -= 1
             if moves > most_moves:
                 most_moves = moves
                 coordinates = [i, j]
